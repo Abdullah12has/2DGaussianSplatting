636c012a32084b5d8b9868df4cbff8dd850cd399
Abdullah Hassan <abdullah.hassan@talentadore.com>
2026-02-01 20:08:05 +0100

    connect user change

--- connect.sh
+++ connect.sh
@@ -8,5 +8,5 @@ chmod 600 "$KEY_FILE"
 
 # SSH into server and allocate GPU
 echo "Connecting to ml3d.vc.in.tum.de..."
-ssh -i "$KEY_FILE" koubaa@ml3d.vc.in.tum.de -t \
+ssh -i "$KEY_FILE" abdullah@ml3d.vc.in.tum.de -t \
     'cd /cluster/51/koubaa/abdullah/2DGaussianSplatting && salloc --gpus=1'
