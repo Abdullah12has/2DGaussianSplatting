78558660c9ef4e6773fe0b3e4ec706291aea81d6
Mahdi Koubaa <mahdi.koubaa@tum.de>
2026-02-12 20:59:24 +0100

    done

--- connect_abdullah.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sshfs -o allow_other,ssh_command='ssh -4' abdullah@ml3d.vc.in.tum.de:/cluster/51/abdullah/2DGaussianSplatting .
-ssh abdullah@ml3d.vc.in.tum.de  -t 'cd /cluster/51/abdullah/2DGaussianSplatting && salloc --gpus=1'
